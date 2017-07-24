7a9d71e25d436ebfd55d722f687e8083279bad2c
martin byrne <marb61a@gmail.com>
2017-07-24 15:45:35 +0000

    added register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component, onInit} from '@angular/core';
+
+@Component({
+    selector: 'app-register',
+    templateUrl: './register/component.html',
+    styleUrls: ['./register/component.css']
+})
+export class RegisterComponent implements onInit{
+    constructor() { }
+    ngOnInit() { }
+}
