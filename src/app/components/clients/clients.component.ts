53a5dd6f7b32e910840453561615890d0548f1f0
martin byrne <marb61a@gmail.com>
2017-09-11 11:25:49 +0000

    updated clients component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-
-  constructor() { }
+  clients:any[];
+  totalOwed:number;
+  
+  constructor(public clientService:ClientService ) { }
 
   ngOnInit() {
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed;
+    })  
+  }
+  
+  getTotalOwed(){
+    let total = 0;
+    for(let i = 0; i < this.clients.length; i++){
+      total += parseFloat(this.clients[i].balance);
+    }
+    
+    this.totalOwed = total;
+    console.log(this.totalOwed);
   }
 
 }
