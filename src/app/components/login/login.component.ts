38217a21f8e5b15dc8af07b73a1dc90cb76467f7
martin byrne <marb61a@gmail.com>
2017-09-21 17:05:09 +0000

    added auth guard

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email:string;
+  password:string;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router:Router,
+    private _flashMessagesService:FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
