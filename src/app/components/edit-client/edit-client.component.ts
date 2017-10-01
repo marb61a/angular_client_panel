439cbee659b339354ea216289581eb2968129478
martin byrne <marb61a@gmail.com>
2017-10-01 17:59:09 +0000

    added edit client ts file

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../services/client.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Client } from '../../models/Client';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-edit-client',
@@ -20,9 +21,35 @@ export class EditClientComponent implements OnInit {
   }
   disableBalanceOnEdit:boolean = true;
   
-  constructor() { }
+  constructor(
+    public clientService:ClientService,
+    public router:Router,
+    public route:ActivatedRoute,
+    public flashMessagesService:FlashMessagesService,
+    public settingsService:SettingsService
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    
+    // Get the client
+    this.clientService.getClient(this.id).subscribe(client => {
+      this.client = client;
+    })
+    
+    this.disableBalanceOnEdit = this.settingsService.getSettings().disableBalanceOnEdit;
+  }
+  
+  onSubmit({value, valid}:{value:Client, valid:boolean}){
+    if(!valid){
+      this.flashMessagesService.show('Please fill in all fields', {cssClass:'alert-danger', timeout: 4000});
+      this.router.navigate(['edit-client/'+this.id]);
+    } else {
+      // Update the client
+      this.clientService.updateClient(this.id, value);
+      this.flashMessagesService.show('Client updated', {cssClass:'alert-success', timeout: 4000});
+      this.router.navigate(['/client/'+this.id]);
+    }
   }
 
 }
